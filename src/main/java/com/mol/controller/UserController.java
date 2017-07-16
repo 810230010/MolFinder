be67a1a721e00a9f49d212db83b2ad11e09dcf66
jjp <810230010@qq.com>
2017-07-16 23:07:13 +0800

    register page

--- src/main/java/com/mol/controller/UserController.java
+++ src/main/java/com/mol/controller/UserController.java
@@ -1,16 +1,10 @@
 package com.mol.controller;
 
-import com.mol.common.Exception.MyException.UnknownAccountException;
 import com.mol.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Arrays;
-import java.util.Map;
 
 /**
  * Created by 江建平 on 2017/7/9.
@@ -21,6 +15,9 @@ public class UserController {
     private static Logger log = Logger.getLogger(UserController.class);
     @Autowired
     private UserService userService;
-
+    @RequestMapping("/registerPage")
+    public String view2register(){
+        return "registery";
+    }
 
 }
